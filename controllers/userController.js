5d03b1e2d24726abe71a219b34148de0c527fa54
saurabhh-svg <singhsaurabh2901@gmail.com>
2023-02-26 01:35:43 +0530

    feat: login and register user using jwt

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,17 +1,68 @@
 const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const User = require("../models/userModel");
 
 //@desc Register a user
 //@route POST /api/users/register
 //@access Public
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Register the User" });
+  const { username, email, password } = req.body;
+  if (!username || !email || !password) {
+    res.status(400);
+    throw new Error("All fields are required");
+  }
+  const userAvailable = await User.findOne({ email });
+  if (userAvailable) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  //hash the password
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  const user = await User.create({ username, email, password: hashedPassword });
+
+  if (user) {
+    res
+      .status(201)
+      .json({ _id: user._id, username: user.username, email: user.email });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
 });
 
 //@desc Login a user
 //@route POST /api/users/login
 //@access Public
 const loginUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Login the User" });
+  const { email, password } = req.body;
+  if (!email || !password) {
+    res.status(400);
+    throw new Error("All fields are required");
+  }
+  const user = await User.findOne({ email });
+  //compare password with the hashed password
+  if (user && (await bcrypt.compare(password, user.password))) {
+    const accessToken = jwt.sign(
+      {
+        user: {
+          username: user.username,
+          email: user.email,
+          id: user._id,
+        },
+      },
+      process.env.ACCESS_TOKEN_SECRET,
+      {
+        expiresIn: "1m",
+      }
+    );
+    res.status(200).json({ accessToken });
+  } else {
+    res.status(400);
+    throw new Error("Invalid email or password");
+  }
 });
 
 //@desc Register a user
---
+++ middleware/validateTokenHandler.js
@@ -0,0 +1,7 @@
+const asyncHandler = require("express-async-handler");
+
+const jwt = require("jsonwebtoken");
+
+const validateTokenHandler = asyncHandler(async (req, res, next) => { 
+    
+});
